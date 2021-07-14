0fce2c99934c174147987896d403984ff67d229e
huberti1 <hubertmisiewicz1@gmail.com>
2021-07-14 14:35:22 +0200

    Add emscripten

---
+++ templates/full/emscripten/build.bat
@@ -0,0 +1,1 @@
+emsdk\emsdk_env.bat & emcc ..\main.cpp ..\vendor\PUGIXML\src\pugixml.cpp -I ..\vendor\PUGIXML\src -O2 -s USE_SDL=2 -s USE_SDL_IMAGE=2 -s "SDL2_IMAGE_FORMATS=['png']" -s USE_SDL_TTF=2 -s USE_SDL_MIXER=2 -s USE_SDL_NET=2 --preload-file ..\res -o index.html & xcopy /Y template.html index.html
