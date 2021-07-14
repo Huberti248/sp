aa3243c233462666cf5eaa26f10e978bda2fb1bd
huberti1 <hubertmisiewicz1@gmail.com>
2021-07-14 15:32:55 +0200

    Add emscripten main loop

--- templates/full/main.cpp
+++ templates/full/main.cpp
@@ -1,40 +1,48 @@
-#include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <vector>
-#include <map>
-#include <unordered_map>
-#include <iomanip>
-#include <filesystem>
-#include <cstdio>
-#include <cmath>
-#include <ctime>
-#include <cstdlib>
 #include <SDL.h>
 #include <SDL_image.h>
-#include <SDL_ttf.h>
 #include <SDL_mixer.h>
 #include <SDL_net.h>
+#include <SDL_ttf.h>
+#include <cmath>
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+#include <filesystem>
+#include <fstream>
+#include <iomanip>
+#include <iostream>
+#include <map>
+#include <sstream>
+#include <string>
+#include <unordered_map>
+#include <vector>
 //#include <SDL_gpu.h>
 //#include <SFML/Network.hpp>
 //#include <SFML/Graphics.hpp>
 #include <algorithm>
-#include <functional>
-#include <thread>
 #include <atomic>
-#include <mutex>
-#include <locale>
 #include <codecvt>
+#include <functional>
+#include <locale>
+#include <mutex>
+#include <thread>
 #ifdef __ANDROID__
+#include "vendor/PUGIXML/src/pugixml.hpp"
 #include <android/log.h> //__android_log_print(ANDROID_LOG_VERBOSE, "AppName_", "Example number log: %d", number);
 #include <jni.h>
-#include "vendor/PUGIXML/src/pugixml.hpp"
 #else
-#include <pugixml.hpp>
 #include <filesystem>
+#include <pugixml.hpp>
+#ifdef __EMSCRIPTEN__
+namespace fs = std::__fs::filesystem;
+#else
 namespace fs = std::filesystem;
+#endif
 using namespace std::chrono_literals;
+#endif
+#ifdef __EMSCRIPTEN__
+#include <emscripten.h>
+#include <emscripten/html5.h>
 #endif
 
 // NOTE: Remember to uncomment it on every release
@@ -55,339 +63,344 @@ SDL_Point mousePos;
 SDL_Point realMousePos;
 bool keys[SDL_NUM_SCANCODES];
 bool buttons[SDL_BUTTON_X2 + 1];
+SDL_Window* window;
 SDL_Renderer* renderer;
+TTF_Font* robotoF;
+bool running = true;
 
 void logOutputCallback(void* userdata, int category, SDL_LogPriority priority, const char* message)
 {
-	std::cout << message << std::endl;
+    std::cout << message << std::endl;
 }
 
 int random(int min, int max)
 {
-	return min + rand() % ((max + 1) - min);
+    return min + rand() % ((max + 1) - min);
 }
 
 int SDL_QueryTextureF(SDL_Texture* texture, Uint32* format, int* access, float* w, float* h)
 {
-	int wi, hi;
-	int result = SDL_QueryTexture(texture, format, access, &wi, &hi);
-	if(w) {
-		*w = wi;
-	}
-	if(h) {
-		*h = hi;
-	}
-	return result;
+    int wi, hi;
+    int result = SDL_QueryTexture(texture, format, access, &wi, &hi);
+    if (w) {
+        *w = wi;
+    }
+    if (h) {
+        *h = hi;
+    }
+    return result;
 }
 
 SDL_bool SDL_PointInFRect(const SDL_Point* p, const SDL_FRect* r)
 {
-	return ((p->x >= r->x) && (p->x < (r->x + r->w)) &&
-		(p->y >= r->y) && (p->y < (r->y + r->h))) ? SDL_TRUE : SDL_FALSE;
+    return ((p->x >= r->x) && (p->x < (r->x + r->w)) && (p->y >= r->y) && (p->y < (r->y + r->h))) ? SDL_TRUE : SDL_FALSE;
 }
 
 std::ostream& operator<<(std::ostream& os, SDL_FRect r)
 {
-	os << r.x << " " << r.y << " " << r.w << " " << r.h;
-	return os;
+    os << r.x << " " << r.y << " " << r.w << " " << r.h;
+    return os;
 }
 
 std::ostream& operator<<(std::ostream& os, SDL_Rect r)
 {
-	SDL_FRect fR;
-	fR.w = r.w;
-	fR.h = r.h;
-	fR.x = r.x;
-	fR.y = r.y;
-	os << fR;
-	return os;
+    SDL_FRect fR;
+    fR.w = r.w;
+    fR.h = r.h;
+    fR.x = r.x;
+    fR.y = r.y;
+    os << fR;
+    return os;
 }
 
 SDL_Texture* renderText(SDL_Texture* previousTexture, TTF_Font* font, SDL_Renderer* renderer, const std::string& text, SDL_Color color)
 {
-	if (previousTexture) {
-		SDL_DestroyTexture(previousTexture);
-	}
-	SDL_Surface* surface;
-	if (text.empty()) {
-		surface = TTF_RenderUTF8_Blended(font, " ", color);
-	}
-	else {
-		surface = TTF_RenderUTF8_Blended(font, text.c_str(), color);
-	}
-	if (surface) {
-		SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
-		SDL_FreeSurface(surface);
-		return texture;
-	}
-	else {
-		return 0;
-	}
+    if (previousTexture) {
+        SDL_DestroyTexture(previousTexture);
+    }
+    SDL_Surface* surface;
+    if (text.empty()) {
+        surface = TTF_RenderUTF8_Blended(font, " ", color);
+    }
+    else {
+        surface = TTF_RenderUTF8_Blended(font, text.c_str(), color);
+    }
+    if (surface) {
+        SDL_Texture* texture = SDL_CreateTextureFromSurface(renderer, surface);
+        SDL_FreeSurface(surface);
+        return texture;
+    }
+    else {
+        return 0;
+    }
 }
 
 struct Text {
-	std::string text;
-	SDL_Surface* surface = 0;
-	SDL_Texture* t = 0;
-	SDL_FRect dstR{};
-	bool autoAdjustW = false;
-	bool autoAdjustH = false;
-	float wMultiplier = 1;
-	float hMultiplier = 1;
+    std::string text;
+    SDL_Surface* surface = 0;
+    SDL_Texture* t = 0;
+    SDL_FRect dstR{};
+    bool autoAdjustW = false;
+    bool autoAdjustH = false;
+    float wMultiplier = 1;
+    float hMultiplier = 1;
 
-	~Text()
-	{
-		if (surface) {
-			SDL_FreeSurface(surface);
-		}
-		if (t) {
-			SDL_DestroyTexture(t);
-		}
-	}
+    ~Text()
+    {
+        if (surface) {
+            SDL_FreeSurface(surface);
+        }
+        if (t) {
+            SDL_DestroyTexture(t);
+        }
+    }
 
-	Text()
-	{
+    Text()
+    {
+    }
 
-	}
+    Text(const Text& rightText)
+    {
+        text = rightText.text;
+        if (surface) {
+            SDL_FreeSurface(surface);
+        }
+        if (t) {
+            SDL_DestroyTexture(t);
+        }
+        if (rightText.surface) {
+            surface = SDL_ConvertSurface(rightText.surface, rightText.surface->format, SDL_SWSURFACE);
+        }
+        if (rightText.t) {
+            t = SDL_CreateTextureFromSurface(renderer, surface);
+        }
+        dstR = rightText.dstR;
+        autoAdjustW = rightText.autoAdjustW;
+        autoAdjustH = rightText.autoAdjustH;
+        wMultiplier = rightText.wMultiplier;
+        hMultiplier = rightText.hMultiplier;
+    }
 
-	Text(const Text& rightText)
-	{
-		text = rightText.text;
-		if (surface) {
-			SDL_FreeSurface(surface);
-		}
-		if (t) {
-			SDL_DestroyTexture(t);
-		}
-		if (rightText.surface) {
-			surface = SDL_ConvertSurface(rightText.surface, rightText.surface->format, SDL_SWSURFACE);
-		}
-		if (rightText.t) {
-			t = SDL_CreateTextureFromSurface(renderer, surface);
-		}
-		dstR = rightText.dstR;
-		autoAdjustW = rightText.autoAdjustW;
-		autoAdjustH = rightText.autoAdjustH;
-		wMultiplier = rightText.wMultiplier;
-		hMultiplier = rightText.hMultiplier;
-	}
+    Text& operator=(const Text& rightText)
+    {
+        text = rightText.text;
+        if (surface) {
+            SDL_FreeSurface(surface);
+        }
+        if (t) {
+            SDL_DestroyTexture(t);
+        }
+        if (rightText.surface) {
+            surface = SDL_ConvertSurface(rightText.surface, rightText.surface->format, SDL_SWSURFACE);
+        }
+        if (rightText.t) {
+            t = SDL_CreateTextureFromSurface(renderer, surface);
+        }
+        dstR = rightText.dstR;
+        autoAdjustW = rightText.autoAdjustW;
+        autoAdjustH = rightText.autoAdjustH;
+        wMultiplier = rightText.wMultiplier;
+        hMultiplier = rightText.hMultiplier;
+        return *this;
+    }
 
-	Text& operator=(const Text& rightText)
-	{
-		text = rightText.text;
-		if (surface) {
-			SDL_FreeSurface(surface);
-		}
-		if (t) {
-			SDL_DestroyTexture(t);
-		}
-		if (rightText.surface) {
-			surface = SDL_ConvertSurface(rightText.surface, rightText.surface->format, SDL_SWSURFACE);
-		}
-		if (rightText.t) {
-			t = SDL_CreateTextureFromSurface(renderer, surface);
-		}
-		dstR = rightText.dstR;
-		autoAdjustW = rightText.autoAdjustW;
-		autoAdjustH = rightText.autoAdjustH;
-		wMultiplier = rightText.wMultiplier;
-		hMultiplier = rightText.hMultiplier;
-		return *this;
-	}
-
-	void setText(SDL_Renderer* renderer, TTF_Font* font, std::string text, SDL_Color c = { 255,255,255 })
-	{
-		this->text = text;
+    void setText(SDL_Renderer* renderer, TTF_Font* font, std::string text, SDL_Color c = { 255, 255, 255 })
+    {
+        this->text = text;
 #if 1 // NOTE: renderText
-		if (surface) {
-			SDL_FreeSurface(surface);
-		}
-		if (t) {
-			SDL_DestroyTexture(t);
-		}
-		if (text.empty()) {
-			surface = TTF_RenderUTF8_Blended(font, " ", c);
-		}
-		else {
-			surface = TTF_RenderUTF8_Blended(font, text.c_str(), c);
-		}
-		if (surface) {
-			t = SDL_CreateTextureFromSurface(renderer, surface);
-		}
+        if (surface) {
+            SDL_FreeSurface(surface);
+        }
+        if (t) {
+            SDL_DestroyTexture(t);
+        }
+        if (text.empty()) {
+            surface = TTF_RenderUTF8_Blended(font, " ", c);
+        }
+        else {
+            surface = TTF_RenderUTF8_Blended(font, text.c_str(), c);
+        }
+        if (surface) {
+            t = SDL_CreateTextureFromSurface(renderer, surface);
+        }
 #endif
-		if (autoAdjustW) {
-			SDL_QueryTextureF(t, 0, 0, &dstR.w, 0);
-		}
-		if (autoAdjustH) {
-			SDL_QueryTextureF(t, 0, 0, 0, &dstR.h);
-		}
-		dstR.w *= wMultiplier;
-		dstR.h *= hMultiplier;
-	}
+        if (autoAdjustW) {
+            SDL_QueryTextureF(t, 0, 0, &dstR.w, 0);
+        }
+        if (autoAdjustH) {
+            SDL_QueryTextureF(t, 0, 0, 0, &dstR.h);
+        }
+        dstR.w *= wMultiplier;
+        dstR.h *= hMultiplier;
+    }
 
-	void setText(SDL_Renderer* renderer, TTF_Font* font, int value, SDL_Color c = { 255,255,255 })
-	{
-		setText(renderer, font, std::to_string(value), c);
-	}
+    void setText(SDL_Renderer* renderer, TTF_Font* font, int value, SDL_Color c = { 255, 255, 255 })
+    {
+        setText(renderer, font, std::to_string(value), c);
+    }
 
-	void draw(SDL_Renderer* renderer)
-	{
-		if(t) {
-			SDL_RenderCopyF(renderer, t, 0, &dstR);
-		}
-	}
+    void draw(SDL_Renderer* renderer)
+    {
+        if (t) {
+            SDL_RenderCopyF(renderer, t, 0, &dstR);
+        }
+    }
 };
 
 int SDL_RenderDrawCircle(SDL_Renderer* renderer, int x, int y, int radius)
 {
-	int offsetx, offsety, d;
-	int status;
+    int offsetx, offsety, d;
+    int status;
 
+    offsetx = 0;
+    offsety = radius;
+    d = radius - 1;
+    status = 0;
 
-	offsetx = 0;
-	offsety = radius;
-	d = radius - 1;
-	status = 0;
+    while (offsety >= offsetx) {
+        status += SDL_RenderDrawPoint(renderer, x + offsetx, y + offsety);
+        status += SDL_RenderDrawPoint(renderer, x + offsety, y + offsetx);
+        status += SDL_RenderDrawPoint(renderer, x - offsetx, y + offsety);
+        status += SDL_RenderDrawPoint(renderer, x - offsety, y + offsetx);
+        status += SDL_RenderDrawPoint(renderer, x + offsetx, y - offsety);
+        status += SDL_RenderDrawPoint(renderer, x + offsety, y - offsetx);
+        status += SDL_RenderDrawPoint(renderer, x - offsetx, y - offsety);
+        status += SDL_RenderDrawPoint(renderer, x - offsety, y - offsetx);
 
-	while (offsety >= offsetx) {
-		status += SDL_RenderDrawPoint(renderer, x + offsetx, y + offsety);
-		status += SDL_RenderDrawPoint(renderer, x + offsety, y + offsetx);
-		status += SDL_RenderDrawPoint(renderer, x - offsetx, y + offsety);
-		status += SDL_RenderDrawPoint(renderer, x - offsety, y + offsetx);
-		status += SDL_RenderDrawPoint(renderer, x + offsetx, y - offsety);
-		status += SDL_RenderDrawPoint(renderer, x + offsety, y - offsetx);
-		status += SDL_RenderDrawPoint(renderer, x - offsetx, y - offsety);
-		status += SDL_RenderDrawPoint(renderer, x - offsety, y - offsetx);
+        if (status < 0) {
+            status = -1;
+            break;
+        }
 
-		if (status < 0) {
-			status = -1;
-			break;
-		}
+        if (d >= 2 * offsetx) {
+            d -= 2 * offsetx + 1;
+            offsetx += 1;
+        }
+        else if (d < 2 * (radius - offsety)) {
+            d += 2 * offsety - 1;
+            offsety -= 1;
+        }
+        else {
+            d += 2 * (offsety - offsetx - 1);
+            offsety -= 1;
+            offsetx += 1;
+        }
+    }
 
-		if (d >= 2 * offsetx) {
-			d -= 2 * offsetx + 1;
-			offsetx += 1;
-		}
-		else if (d < 2 * (radius - offsety)) {
-			d += 2 * offsety - 1;
-			offsety -= 1;
-		}
-		else {
-			d += 2 * (offsety - offsetx - 1);
-			offsety -= 1;
-			offsetx += 1;
-		}
-	}
-
-	return status;
+    return status;
 }
-
 
 int SDL_RenderFillCircle(SDL_Renderer* renderer, int x, int y, int radius)
 {
-	int offsetx, offsety, d;
-	int status;
+    int offsetx, offsety, d;
+    int status;
 
+    offsetx = 0;
+    offsety = radius;
+    d = radius - 1;
+    status = 0;
 
-	offsetx = 0;
-	offsety = radius;
-	d = radius - 1;
-	status = 0;
+    while (offsety >= offsetx) {
 
-	while (offsety >= offsetx) {
+        status += SDL_RenderDrawLine(renderer, x - offsety, y + offsetx,
+            x + offsety, y + offsetx);
+        status += SDL_RenderDrawLine(renderer, x - offsetx, y + offsety,
+            x + offsetx, y + offsety);
+        status += SDL_RenderDrawLine(renderer, x - offsetx, y - offsety,
+            x + offsetx, y - offsety);
+        status += SDL_RenderDrawLine(renderer, x - offsety, y - offsetx,
+            x + offsety, y - offsetx);
 
-		status += SDL_RenderDrawLine(renderer, x - offsety, y + offsetx,
-			x + offsety, y + offsetx);
-		status += SDL_RenderDrawLine(renderer, x - offsetx, y + offsety,
-			x + offsetx, y + offsety);
-		status += SDL_RenderDrawLine(renderer, x - offsetx, y - offsety,
-			x + offsetx, y - offsety);
-		status += SDL_RenderDrawLine(renderer, x - offsety, y - offsetx,
-			x + offsety, y - offsetx);
+        if (status < 0) {
+            status = -1;
+            break;
+        }
 
-		if (status < 0) {
-			status = -1;
-			break;
-		}
+        if (d >= 2 * offsetx) {
+            d -= 2 * offsetx + 1;
+            offsetx += 1;
+        }
+        else if (d < 2 * (radius - offsety)) {
+            d += 2 * offsety - 1;
+            offsety -= 1;
+        }
+        else {
+            d += 2 * (offsety - offsetx - 1);
+            offsety -= 1;
+            offsetx += 1;
+        }
+    }
 
-		if (d >= 2 * offsetx) {
-			d -= 2 * offsetx + 1;
-			offsetx += 1;
-		}
-		else if (d < 2 * (radius - offsety)) {
-			d += 2 * offsety - 1;
-			offsety -= 1;
-		}
-		else {
-			d += 2 * (offsety - offsetx - 1);
-			offsety -= 1;
-			offsetx += 1;
-		}
-	}
-
-	return status;
+    return status;
 }
 
 int eventWatch(void* userdata, SDL_Event* event)
 {
-	// WARNING: Be very careful of what you do in the function, as it may run in a different thread
-	if (event->type == SDL_APP_TERMINATING || event->type == SDL_APP_WILLENTERBACKGROUND) {
+    // WARNING: Be very careful of what you do in the function, as it may run in a different thread
+    if (event->type == SDL_APP_TERMINATING || event->type == SDL_APP_WILLENTERBACKGROUND) {
+    }
+    return 0;
+}
 
-	}
-	return 0;
+void mainLoop()
+{
+    SDL_Event event;
+    while (SDL_PollEvent(&event)) {
+        if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
+            running = false;
+            // TODO: On mobile remember to use eventWatch function (it doesn't reach this code when terminating)
+        }
+        if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED) {
+            SDL_RenderSetScale(renderer, event.window.data1 / (float)windowWidth, event.window.data2 / (float)windowHeight);
+        }
+        if (event.type == SDL_KEYDOWN) {
+            keys[event.key.keysym.scancode] = true;
+        }
+        if (event.type == SDL_KEYUP) {
+            keys[event.key.keysym.scancode] = false;
+        }
+        if (event.type == SDL_MOUSEBUTTONDOWN) {
+            buttons[event.button.button] = true;
+        }
+        if (event.type == SDL_MOUSEBUTTONUP) {
+            buttons[event.button.button] = false;
+        }
+        if (event.type == SDL_MOUSEMOTION) {
+            float scaleX, scaleY;
+            SDL_RenderGetScale(renderer, &scaleX, &scaleY);
+            mousePos.x = event.motion.x / scaleX;
+            mousePos.y = event.motion.y / scaleY;
+            realMousePos.x = event.motion.x;
+            realMousePos.y = event.motion.y;
+        }
+    }
+    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
+    SDL_RenderClear(renderer);
+    SDL_RenderPresent(renderer);
 }
 
 int main(int argc, char* argv[])
 {
-	std::srand(std::time(0));
-	SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
-	SDL_LogSetOutputFunction(logOutputCallback, 0);
-	SDL_Init(SDL_INIT_EVERYTHING);
-	TTF_Init();
-	SDL_GetMouseState(&mousePos.x, &mousePos.y);
-	SDL_Window* window = SDL_CreateWindow("AppName_", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, SDL_WINDOW_RESIZABLE);
-	renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-	TTF_Font* robotoF = TTF_OpenFont("res/roboto.ttf", 72);
-	int w, h;
-	SDL_GetWindowSize(window, &w, &h);
-	SDL_RenderSetScale(renderer, w / (float)windowWidth, h / (float)windowHeight);
-	SDL_AddEventWatch(eventWatch, 0);
-	bool running = true;
-	while (running) {
-		SDL_Event event;
-		while (SDL_PollEvent(&event)) {
-			if (event.type == SDL_QUIT || event.type == SDL_KEYDOWN && event.key.keysym.scancode == SDL_SCANCODE_ESCAPE) {
-				running = false;
-				// TODO: On mobile remember to use eventWatch function (it doesn't reach this code when terminating)
-			}
-			if (event.type == SDL_WINDOWEVENT && event.window.event == SDL_WINDOWEVENT_RESIZED) {
-				SDL_RenderSetScale(renderer, event.window.data1 / (float)windowWidth, event.window.data2 / (float)windowHeight);
-			}
-			if (event.type == SDL_KEYDOWN) {
-				keys[event.key.keysym.scancode] = true;
-			}
-			if (event.type == SDL_KEYUP) {
-				keys[event.key.keysym.scancode] = false;
-			}
-			if (event.type == SDL_MOUSEBUTTONDOWN) {
-				buttons[event.button.button] = true;
-			}
-			if (event.type == SDL_MOUSEBUTTONUP) {
-				buttons[event.button.button] = false;
-			}
-			if (event.type == SDL_MOUSEMOTION) {
-				float scaleX, scaleY;
-				SDL_RenderGetScale(renderer, &scaleX, &scaleY);
-				mousePos.x = event.motion.x / scaleX;
-				mousePos.y = event.motion.y / scaleY;
-				realMousePos.x = event.motion.x;
-				realMousePos.y = event.motion.y;
-			}
-		}
-		SDL_SetRenderDrawColor(renderer, 0, 0, 0, 0);
-		SDL_RenderClear(renderer);
-		SDL_RenderPresent(renderer);
-	}
-	// TODO: On mobile remember to use eventWatch function (it doesn't reach this code when terminating)
-	return 0;
+    std::srand(std::time(0));
+    SDL_LogSetAllPriority(SDL_LOG_PRIORITY_VERBOSE);
+    SDL_LogSetOutputFunction(logOutputCallback, 0);
+    SDL_Init(SDL_INIT_EVERYTHING);
+    TTF_Init();
+    SDL_GetMouseState(&mousePos.x, &mousePos.y);
+    window = SDL_CreateWindow("AppName_", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, windowWidth, windowHeight, SDL_WINDOW_RESIZABLE);
+    renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    robotoF = TTF_OpenFont("res/roboto.ttf", 72);
+    int w, h;
+    SDL_GetWindowSize(window, &w, &h);
+    SDL_RenderSetScale(renderer, w / (float)windowWidth, h / (float)windowHeight);
+    SDL_AddEventWatch(eventWatch, 0);
+#ifdef __EMSCRIPTEN__
+    emscripten_set_main_loop(mainLoop, 0, 1);
+#else
+    while (running) {
+        mainLoop();
+    }
+#endif
+    // TODO: On mobile remember to use eventWatch function (it doesn't reach this code when terminating)
+    return 0;
 }
